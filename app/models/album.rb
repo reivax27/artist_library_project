6eb65c18249a0b26abc773093ca8925472575112
Xavier Macalalad <johnxavierm47@gmail.com>
2019-02-13 20:27:59 -0600

    Migrated the db for the Album model, added and tested the validation.

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :artist
+
+  validates :name, :units_sold, presence: true
+  validates :units_sold, numericality: { only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_021325) do
+ActiveRecord::Schema.define(version: 2019_02_14_022243) do
+
+  create_table "albums", force: :cascade do |t|
+    t.string "name"
+    t.integer "units_sold"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_albums_on_artist_id"
+  end
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
