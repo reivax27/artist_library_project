1125f7bccf331508c86234a17e645b639e88a466
Xavier Macalalad <johnxavierm47@gmail.com>
2019-02-14 01:42:54 -0600

    Implemented a simple search that searches through 3 of the tables just using the name/title column of the respective tables. Also modified the Search view to display the results of the query. The search is also present throughout all pages of the web page.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,8 @@
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @artists = Artist.where('name LIKE ?', "%#{@query}%")
+    @albums = Album.where('name LIKE ?', "%#{@query}%")
+    @songs = Song.where('title LIKE ?', "%#{@query}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'search/results'
   resources :pages
   resources :artists, only: [:index, :show]
   resources :songs, only: [:index, :show]
   resources :albums, only: [:index, :show]
 
   get 'static/:permalink', to: 'pages#static', as: 'static'
+  get 'search_results', to: 'search#results', as: 'search_results'
 
   root to: 'artists#index'
 end
